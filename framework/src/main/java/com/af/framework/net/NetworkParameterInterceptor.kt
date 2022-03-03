6e14189a81b1843cc48c7969c65fe8b2f2ab237d
Aihui Mao <mahsky@qq.com>
2022-03-03 19:14:02 +0800

    完善公共网络请求参数拦截器功能

--- framework/src/main/java/com/af/framework/net/NetworkParameterInterceptor.kt
+++ framework/src/main/java/com/af/framework/net/NetworkParameterInterceptor.kt
@@ -1,23 +1,43 @@
 package com.af.framework.net
 
-import okhttp3.Interceptor
-import okhttp3.Response
+import okhttp3.*
+
 
 /**
  * Created by mah on 2022/3/2.
  */
 class NetworkParameterInterceptor(private val networkParameterConfig: NetworkParameterAdapter) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val parameter = networkParameterConfig.getGetParameter(request)
-
+        var request = chain.request()
         val urlBuilder = request.url.newBuilder()
-        parameter.forEach {
-            urlBuilder.addQueryParameter(it.key, it.value)
+        when (request.method) {
+            "GET" -> {
+                val parameter = networkParameterConfig.getGetParameter(request)
+                parameter.forEach {
+                    urlBuilder.addQueryParameter(it.key, it.value)
+                }
+                request.newBuilder().url(urlBuilder.build()).build()
+            }
+            "POST" -> {
+                val queryParameter = networkParameterConfig.getPostQueryParameter(request)
+                queryParameter.forEach {
+                    urlBuilder.addQueryParameter(it.key, it.value)
+                }
+                request = request.newBuilder().url(urlBuilder.build()).build()
+                if (request.body is FormBody) {
+                    val builder = FormBody.Builder()
+                    val body = request.body as FormBody
+                    for (i in 0 until body.size) {
+                        builder.add(body.encodedName(i), body.encodedValue(i))
+                    }
+                    val fieldParameter = networkParameterConfig.getPostFieldParameter(request)
+                    fieldParameter.forEach {
+                        builder.add(it.key, it.value)
+                    }
+                    request = request.newBuilder().post(builder.build()).build()
+                }
+            }
         }
-        val newRequest = request.newBuilder()
-            .method(request.method, request.body)
-            .url(urlBuilder.build()).build()
-        return chain.proceed(newRequest)
+        return chain.proceed(request)
     }
 }
