7353615180c0ee702653692ee9d6f0207d5a7d96
Aihui Mao <mahsky@qq.com>
2022-03-15 16:33:34 +0800

    协成全局异常处理

--- app/src/main/java/com/example/af/GlobalCoroutineExceptionHandler.kt
+++ app/src/main/java/com/example/af/GlobalCoroutineExceptionHandler.kt
@@ -1,7 +1,17 @@
 package com.example.af
 
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlin.coroutines.CoroutineContext
+
 /**
  * Created by mah on 2022/3/15.
+ * 协成异常全局handler
  */
-class GlobalCoroutineExceptionHandler {
+class GlobalCoroutineExceptionHandler : CoroutineExceptionHandler {
+    override val key: CoroutineContext.Key<*> = CoroutineExceptionHandler
+
+    override fun handleException(context: CoroutineContext, exception: Throwable) {
+        println("--GlobalCoroutineExceptionHandler--")
+        exception.printStackTrace()
+    }
 }
