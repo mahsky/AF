511e431e653b747aafc39bb5b04c0d0768264fe0
Aihui Mao <mahsky@qq.com>
2022-07-08 16:16:25 +0800

    基本逻辑

--- app/src/main/java/com/example/af/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/af/main/ui/MainActivity.kt
@@ -1,61 +1,135 @@
 package com.example.af.main.ui
 
-import android.annotation.SuppressLint
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.af.framework.ui.BaseActivity
 import com.example.af.databinding.ActivityMainBinding
+import com.example.af.databinding.ItemAppBinding
 import com.example.af.main.viewmodel.MainViewModel
 import com.github.promeg.pinyinhelper.Pinyin
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
 
 class MainActivity : BaseActivity() {
     private lateinit var viewBinding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var appAdapter: AppAdapter
+    private val apps = mutableListOf<App>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(viewBinding.root)
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                /**
-                 * UiState diffing：UiState 对象中的字段越多，数据流就越有可能因为其中一个字段被更新而发出。
-                 * 由于视图没有 diffing 机制来了解连续发出的数据流是否相同，因此每次发出都会导致视图更新。
-                 * 这意味着，可能必须要对 LiveData 使用 Flow API 或 distinctUntilChanged() 等方法来缓解这个问题。
-                 */
-                viewModel.uiState.map { it.name }
-                    .distinctUntilChanged()
-                    .collectLatest {
-                        appendText(it)
-                    }
+        initView()
 
-                viewModel.uiState.collectLatest {
-                    appendText(it.name)
+        lifecycleScope.launch {
+            apps.clear()
+            apps.addAll(getApps(baseContext.packageManager))
+            apps.forEach {
+                println("=: ${it.appName} ${it.letterAppName}")
+            }
+            appAdapter.setApps(apps)
+        }
+
+        viewBinding.editText.doOnTextChanged { text, start, before, count ->
+            println("text: $text")
+            findApp(text)
+        }
+        viewBinding.editText.requestFocus()
+    }
+
+    private fun findApp(text: CharSequence?) {
+        text ?: return
+        val tempApps = mutableListOf<App>()
+        apps.forEach { app ->
+            app.sort = 0
+            var isContains = true
+            text.toString().uppercase(Locale.ENGLISH).toCharArray().forEach { char ->
+                if (isContains && !app.letterAppName.contains(char)) {
+                    isContains = false
+                }
+                if (isContains) {
+                    val index = app.letterAppName.uppercase(Locale.ENGLISH).indexOf(char)
+                    if (index != -1) {
+                        app.sort = app.sort + index
+                    }
+                }
+            }
+            if (isContains) {
+                tempApps.add(app)
+            }
+
+            tempApps.sortBy { it.sort }
+        }
+        appAdapter.setApps(tempApps)
+    }
+
+    private fun initView() {
+        appAdapter = AppAdapter(mutableListOf())
+        viewBinding.recyclerView.layoutManager = GridLayoutManager(this, 4)
+        viewBinding.recyclerView.adapter = appAdapter
+    }
+
+    private suspend fun getApps(packageManager: PackageManager): List<App> = withContext(Dispatchers.IO) {
+        val apps = packageManager.getInstalledPackages(PackageManager.GET_ACTIVITIES)
+        mutableListOf<App>().apply {
+            apps.forEach {
+                if (packageManager.getLaunchIntentForPackage(it.packageName) != null) {
+                    val appName = it.applicationInfo.loadLabel(packageManager).toString()
+                    val pinyin = Pinyin.toPinyin(appName, "")
+                    add(App(it, pinyin, appName))
                 }
             }
         }
-
-        viewBinding.textView.setOnClickListener {
-            viewModel.load()
-        }
-
-        hanziToPinyin()
-    }
-
-    private fun hanziToPinyin() {
-//        Pinyin.init(Pinyin.newConfig().with(CnCityDict.getInstance())
-        val s = Pinyin.toPinyin("Pin Yin 汉子拼音", "")
-        println("Pinyin: $s")
-    }
-
-    @SuppressLint("SetTextI18n")
-    fun appendText(text: String) {
-        viewBinding.textView.text = "${viewBinding.textView.text} + $text"
     }
 }
+
+class AppAdapter(
+    private val orders: MutableList<App>,
+) : RecyclerView.Adapter<AppRender>() {
+
+    fun setApps(orders: List<App>) {
+        this.orders.clear()
+        this.orders.addAll(orders)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppRender {
+        val viewBinding = ItemAppBinding.inflate(LayoutInflater.from(parent.context))
+        return AppRender(viewBinding)
+    }
+
+    override fun getItemCount(): Int = orders.size
+
+    override fun onBindViewHolder(holder: AppRender, position: Int) {
+        holder.render(orders[position], position)
+    }
+}
+
+class AppRender(
+    private val viewBinding: ItemAppBinding,
+) : RecyclerView.ViewHolder(viewBinding.root) {
+    fun render(app: App, position: Int) {
+        viewBinding.name.text = app.appName
+        viewBinding.imageView.setImageDrawable(app.packageInfo.applicationInfo.loadIcon(viewBinding.root.context.packageManager))
+
+        viewBinding.root.setOnClickListener {
+            viewBinding.root.context.startActivity(
+                viewBinding.root.context.packageManager.getLaunchIntentForPackage(app.packageInfo.packageName)
+            )
+        }
+    }
+}
+
+data class App(val packageInfo: PackageInfo, val letterAppName: String, val appName: String, var sort: Int = 0)
